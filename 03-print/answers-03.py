c428ebe4493b0881615500bbc9e583161b997c9f
Umidbek <mrumidjon2020@gmail.com>
2021-07-09 20:35:43 +0500

    new codes

---
+++ 03-print/answers-03.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  4 13:45:55 2021
+
+@author: WINDOWS 10
+"""
+
+#1. Quyidagi matinni aynan manashundy ko'rinishda konsolga chiqaring.
+print("\"Nexia\", \"Tico\", \"Damas\" ko'rganlar qilar havas.")
+
+#2. 5 ning 4-darajasini toping.
+print(f"5^4 = {5**4}")
+
+#3.  22 ni 4 ga bo'lgandagi qoldiqni toping.
+print(f"22 % 4 = {22 % 4}")
+
+#4. Tomni 125 ga teng bo'lgan kvadratning yuzi va perimetrini toping?
+a = 125
+s = a * a
+p = 4 * a 
+print("S =", s)
+print("P =", p)
+
+#5 Diametri 12 ga teng bo'lgan doiraning yuzini toping.
+d = 12
+r = d / 2
+PI = 3.14
+s_doira = PI * r**2
+print("S =", s_doira)
+
+#6. Katetlari 5 va 6 ga teng bo'lgan tog'ri burchakli uchburchakning gipotenuzasini toping
+k1 = 6
+k2 = 7
+g = (6*6 + 7*7)**(1/2)
+print("Gipotenuza:", g)
---
+++ 04-variables/answers-04.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  4 14:10:32 2021
+
+@author: WINDOWS 10
+"""
+
+#1. "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring.
+a = "Hello World!"
+print(a)
+
+#2. xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring,
+# keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar = "Bugun darsga borasanmi?"
+print(xabar)
+xabar = "Bugun darsga bora olmayman."
+print(xabar)
+
+#3. Quyidagi kodni bajaring.
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi = ", aylana_yuzi)
