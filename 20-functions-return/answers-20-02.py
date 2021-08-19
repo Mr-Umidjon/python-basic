734bcdc7ae1bd9ac4ab554b31036e24a3a388574
Umidbek <mrumidjon2020@gmail.com>
2021-08-19 10:28:08 +0500

    Update answers-20-02.py

--- 20-functions-return/answers-20-02.py
+++ 20-functions-return/answers-20-02.py
@@ -17,7 +17,7 @@ def user_info(ism, familiya, tyil, tjoy, yosh, email=None, telefon=None):
             "yosh":yosh ,
             'email':email,
             "telefon":telefon       
-        }
+            }
     return info
 user = user_info("ali", "valiyev", 1987, "andijon", 23)
 
@@ -25,22 +25,22 @@ user = user_info("ali", "valiyev", 1987, "andijon", 23)
 # degan ro'yxatni shakllantiring. Ro'yxatdagi mijozlar haqidagi ma'lumotni
 # konsolga chiqaring.
 mijozlar = []
-# while True:
-#     print("\nMijoz haqida quyidagi ma'lumotlarni kiriting ")
-#     ism = input("Ismi: ")
-#     familiya = input("Familiyasi: ")
-#     tyil = int(input("Tug'ilgan yili:"))
-#     tjoy = input("Tug'ilgan joyi: ")
-#     yosh = int(input("Yoshi: "))
-#     email = input("Email manzili: ")
-#     telefon = input("Telefon raqami: ")
-#     mijozlar.append(user_info(ism, familiya, tyil, tjoy, yosh, email, telefon))
-#     javob = input("Yana mijoz haqida ma'lumot kiritasizmi(yes/no)")
-#     if javob == 'no':
-#         break
-# for mijoz in mijozlar:
-#     info = f"{mijoz['ism'].title()} {mijoz['familiya'].title()} "
-#     info += f"{mijoz['tyil']}-yilda {mijoz['tjoy'].title()}da tug'ilgan. "
+while True:
+    print("\nMijoz haqida quyidagi ma'lumotlarni kiriting ")
+    ism = input("Ismi: ")
+    familiya = input("Familiyasi: ")
+    tyil = int(input("Tug'ilgan yili:"))
+    tjoy = input("Tug'ilgan joyi: ")
+    yosh = int(input("Yoshi: "))
+    email = input("Email manzili: ")
+    telefon = input("Telefon raqami: ")
+    mijozlar.append(user_info(ism, familiya, tyil, tjoy, yosh, email, telefon))
+    javob = input("Yana mijoz haqida ma'lumot kiritasizmi(yes/no)")
+    if javob == 'no':
+        break
+for mijoz in mijozlar:
+    info = f"{mijoz['ism'].title()} {mijoz['familiya'].title()} "
+    info += f"{mijoz['tyil']}-yilda {mijoz['tjoy'].title()}da tug'ilgan. "
 
 
 # Uchta son qabul qilib, ulardan eng kattasini qaytaruvchi funksiya yozing
@@ -72,6 +72,24 @@ def aylana(radius):
     return aylana
     
 
-# Berilgan oraliqdagi tub sonlar ro'yxatini qaytaruvchi funksiya yozing (tub sonlar —faqat birga va o'ziga qoldiqsiz bo'linuvchi, 1 dan katta musbat sonlar)
-
+# Berilgan oraliqdagi tub sonlar ro'yxatini qaytaruvchi funksiya yozing 
+# (tub sonlar — faqat birga va o'ziga qoldiqsiz bo'linuvchi, 1 dan katta musbat sonlar)
+def tub_son(a, b):
+    """Berilgan oraliqdagi tub sonlar ro'yxatini qaytaruvchi funksiya"""
+    tub_sonlar = []
+    for son in range(a, b+1):
+        tub = True
+        if son == 1:
+            tub = False
+        elif son == 2:
+            tub = True
+        else:
+            for x in range(2, son):
+                if son%x == 0:
+                    tub = False
+        if tub:
+            tub_sonlar.append(son)
+    return tub_sonlar
+        
+        
 # Foydalanuvchidan son qabul qilib, shu son miqdoricha Fibonachchi ketma-ketligidagi sonlar ro'yxatni qaytaruvchi funksiya yozing. Ta’rif: Har bir hadi o’zidan oldingi ikkita hadning yig’indisiga teng bo’lgan ketma-ketlik Fibonachchi ketma-ketligi deyiladi. Bunda boshlang’ish had ko’pincha 1 deb olinadi. 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,.
