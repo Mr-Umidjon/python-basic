fb62809714bc3f6f2980f7a02b9e06b59a218e4a
Umidbek <mrumidjon2020@gmail.com>
2021-08-22 14:03:28 +0500

    Create answers-22-02.py

---
+++ 22-functions-args-kwargs/answers-22-02.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 22 13:53:42 2021
+
+@author: WINDOWS 10
+"""
+
+# Talabalar haqidagi ma'lumotlarini lug'at ko'rinishida qaytaruvchi funkisya
+# yozing. Talabaning ismi va familiyasi majburiy argument, qolgan
+# ma'lumotlar esa ixtiyoriy ko'rinishda istalgancha berilishi mumkin bo'lsin.
+def talaba(ism, familiya, **malumotlar):
+    """Talabalar haqidagi ma'lumotlarini lug'at ko'rinishida qaytaruvchi funkisya """
+    malumotlar['ism'] = ism
+    malumotlar['familiya'] = familiya
+    return malumotlar
